2eb2c734ee4bc36ac55dfb06739c798fb2daa513
Yuma Satake <satous.y@icloud.com>
2024-10-31 01:57:34 +0900

    feat: 戻るボタンを追加

--- frontend_v2/src/components/header/Header.tsx
+++ frontend_v2/src/components/header/Header.tsx
@@ -1,11 +1,19 @@
 import { useAuth } from '@/store/useAuth';
-import { Avatar, Button, Stack, Typography } from '@mui/material';
+import { Avatar, Button, IconButton, Stack, Typography } from '@mui/material';
 import type { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
 export const Header: FC = () => {
+  const { pathname } = useLocation();
   const router = useNavigate();
   const { user } = useAuth();
+
+  const isHiddenBackButton = pathname === '/';
+
+  const handleBack = (): void => {
+    router(-1);
+  };
 
   const handleBackHome = (): void => {
     router('/');
@@ -16,10 +24,25 @@ export const Header: FC = () => {
   };
 
   return (
-    <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ py: 2 }}>
-      <div />
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        py: 2,
+        /* ヘッダーの操作系を微妙に内側に寄せるための余白 */
+        px: 0.5,
+      }}
+    >
+      <IconButton
+        onClick={handleBack}
+        disabled={isHiddenBackButton}
+        sx={{ opacity: isHiddenBackButton ? 0 : 1 }}
+      >
+        <KeyboardBackspaceIcon fontSize="medium" sx={{ color: 'white' }} />
+      </IconButton>
       <Button onClick={handleBackHome} variant="text">
-        <Typography variant="h5" color="white">
+        <Typography variant="h5" color="white" sx={{ textTransform: 'none', fontWeight: 'bold' }}>
           Submarine
         </Typography>
       </Button>
