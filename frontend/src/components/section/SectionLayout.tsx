39c334deff5c1be1762344a83989f05d27a6de4d
Yuma Satake <satous.y@icloud.com>
2024-10-31 17:49:56 +0900

    feat: ヘルプボタンのコンポーネント実装

--- frontend/src/components/section/SectionLayout.tsx
+++ frontend/src/components/section/SectionLayout.tsx
@@ -1,16 +1,25 @@
-import { Stack, SxProps, Typography } from '@mui/material';
+import { Stack, SxProps, Tooltip, Typography } from '@mui/material';
 import { FC, ReactNode } from 'react';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 type Props = {
   sectionTitle: string;
   children: ReactNode;
+  helpText?: string;
   sx?: SxProps;
 };
 
-export const SectionLayout: FC<Props> = ({ sectionTitle, children, sx }) => {
+export const SectionLayout: FC<Props> = ({ sectionTitle, children, helpText, sx }) => {
   return (
     <Stack sx={{ width: '100%', pt: 3, ...sx }} spacing={1}>
-      <Typography color="gray">{sectionTitle}</Typography>
+      <Stack direction="row" alignItems="center" spacing={1}>
+        <Typography color="gray">{sectionTitle}</Typography>
+        {helpText ? (
+          <Tooltip title={helpText}>
+            <HelpOutlineIcon fontSize="small" sx={{ color: 'gray' }} />
+          </Tooltip>
+        ) : null}
+      </Stack>
       <div>{children}</div>
     </Stack>
   );
