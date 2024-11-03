7df1d704af318182ea1713c7adcf879574ffccbe
Yuma Satake <satous.y@icloud.com>
2024-11-03 10:46:53 +0900

    fix: デスクトップ画面でトップ画面で登録できるサブスクの候補が多い時にスクロール

--- frontend/src/feature/index/add/AddSubscription.tsx
+++ frontend/src/feature/index/add/AddSubscription.tsx
@@ -40,6 +40,7 @@ export const AddSubscription: FC<Props> = ({
           pointerEvents: 'auto',
           width: '100%',
           height: 100,
+          maxWidth: 'sm',
         }}
       >
         <SquareCard
