d30f7cf695ae7e3d6d45ec65c1837de94312fe80
Yuma Satake <satous.y@icloud.com>
2024-11-05 10:33:20 +0900

    feat: ツールチップのモバイル対応

--- frontend/src/components/section/SectionLayout.tsx
+++ frontend/src/components/section/SectionLayout.tsx
@@ -1,6 +1,7 @@
+import { theme } from '@/theme/theme';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import { Stack, SxProps, Tooltip, Typography } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { Stack, SxProps, Tooltip, Typography, useMediaQuery } from '@mui/material';
+import { FC, ReactNode, useState } from 'react';
 
 type Props = {
   children: ReactNode;
@@ -17,15 +18,28 @@ export const SectionLayout: FC<Props> = ({
   sx,
   titleColor = 'gray',
 }) => {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [tooltipIsOpen, setTooltipIsOpen] = useState(false);
+
   return (
     <Stack sx={{ width: '100%', pb: 3, ...sx }} spacing={1}>
       {sectionTitle || helpText ? (
-        <Stack direction="row" alignItems="center" spacing={1}>
+        <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1 }}>
           {sectionTitle ? <Typography color={titleColor}>{sectionTitle}</Typography> : null}
           {helpText ? (
-            <Tooltip title={helpText}>
-              <HelpOutlineIcon fontSize="small" sx={{ color: titleColor }} />
-            </Tooltip>
+            isSmallScreen ? (
+              <Tooltip
+                title={helpText}
+                open={tooltipIsOpen}
+                onClick={() => setTooltipIsOpen(!tooltipIsOpen)}
+              >
+                <HelpOutlineIcon fontSize="small" sx={{ color: titleColor }} />
+              </Tooltip>
+            ) : (
+              <Tooltip title={helpText}>
+                <HelpOutlineIcon fontSize="small" sx={{ color: titleColor }} />
+              </Tooltip>
+            )
           ) : null}
         </Stack>
       ) : null}
