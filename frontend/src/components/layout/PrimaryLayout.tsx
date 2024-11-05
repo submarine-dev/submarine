a42fc720fb9a182e4332d4024c3713e3fe543cd7
Yuma Satake <satous.y@icloud.com>
2024-11-05 10:07:23 +0900

    fix: モバイルで背景がスクロールしないように修正

--- frontend/src/components/layout/PrimaryLayout.tsx
+++ frontend/src/components/layout/PrimaryLayout.tsx
@@ -15,7 +15,7 @@ const Background: FC = () => {
           top: 0,
           left: 0,
           width: '100%',
-          height: '40dvh',
+          height: '40vh',
           zIndex: -1,
           background: 'linear-gradient(0deg, #0B1C31 0%, #2850BF 100%)',
           borderRadius: '0 0 2% 2%',
@@ -24,10 +24,10 @@ const Background: FC = () => {
       <Box
         sx={{
           position: 'fixed',
-          top: '40dvh',
+          top: '40vh',
           left: 0,
           width: '100%',
-          height: '60dvh',
+          height: '60vh',
           zIndex: -1,
           bgcolor: '#F5F5F5',
         }}
